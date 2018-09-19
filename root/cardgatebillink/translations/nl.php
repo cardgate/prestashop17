821045e5deff4b79cc9486d06cfeed8bb90abf60
richard <richard@richard-imac2.dbs.dbcorp.nl>
2018-09-19 12:35:10 +0200

    Version 1.7.5 Added new payment method: Billink. Added new clientlib

---
+++ root/cardgatebillink/translations/nl.php
@@ -0,0 +1,11 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{cardgatebillink}prestashop>cardgatebillink_da9c72b9e543135f3f59e3c8ac68ef35'] = 'Betaal met ';
+$_MODULE['<{cardgatebillink}prestashop>cardgatebillink_5a49009255576b510d47531dfec6bf18'] = 'CardGate Billink';
+$_MODULE['<{cardgatebillink}prestashop>cardgatebillink_8295202f55258ac1cb55a3f596fb4268'] = 'Accepteert betalingen met CardGate Billink.';
+$_MODULE['<{cardgatebillink}prestashop>cardgatebillink_69a1a3ad8dd5da6db3c4da838a0cf9c7'] = 'Weet u zeker dat u uw details wilt verwijderen?';
+$_MODULE['<{cardgatebillink}prestashop>cardgatebillink_a02758d758e8bec77a33d7f392eb3f8a'] = 'Er is geen valuta ingesteld voor deze module.';
+$_MODULE['<{cardgatebillink}prestashop>cardgatebillink_59ca7aaff7f6bfd9dd776a6add44770f'] = 'De CardGate module is niet gevonden';
+$_MODULE['<{cardgatebillink}prestashop>cardgatebillink_c888438d14855d7d96a2724ee9c306bd'] = 'Instellingen aangepast.';
