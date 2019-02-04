6a7776b0a157c8b90a42a305e4f9f2bfbca13831
richard <richard@richard.dbs.dbcorp.nl>
2019-02-04 13:23:14 +0100

    New payment methods: Paysafecash, Gift Card. Clientlib files updated.

---
+++ root/cardgatepaysafecash/translations/nl.php
@@ -0,0 +1,11 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{cardgatepaysafecash}prestashop>cardgatepaysafecash_da9c72b9e543135f3f59e3c8ac68ef35'] = 'Betaal met ';
+$_MODULE['<{cardgatepaysafecash}prestashop>cardgatepaysafecash_f01e89385a14b8d73157d49e4cbafbbe'] = 'CardGate Paysafecash';
+$_MODULE['<{cardgatepaysafecash}prestashop>cardgatepaysafecash_9bc3bafc70bb036196177de8ee6f5022'] = 'Accepteert betalingen met CardGate Paysafecash';
+$_MODULE['<{cardgatepaysafecash}prestashop>cardgatepaysafecash_69a1a3ad8dd5da6db3c4da838a0cf9c7'] = 'Weet u zeker dat u uw details wilt verwijderen?';
+$_MODULE['<{cardgatepaysafecash}prestashop>cardgatepaysafecash_a02758d758e8bec77a33d7f392eb3f8a'] = 'Er is geen valuta ingesteld voor deze module.';
+$_MODULE['<{cardgatepaysafecash}prestashop>cardgatepaysafecash_59ca7aaff7f6bfd9dd776a6add44770f'] = 'De CardGate module is niet gevonden';
+$_MODULE['<{cardgatepaysafecash}prestashop>cardgatepaysafecash_c888438d14855d7d96a2724ee9c306bd'] = 'Instellingen aangepast.';
