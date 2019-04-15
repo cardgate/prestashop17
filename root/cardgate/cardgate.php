8ebe5425797883b348afb2fbfe0a8259ece6f280
richard <richard@richard.dbs.dbcorp.nl>
2019-04-15 16:36:36 +0200

    Added cache for iDEAL issuers.

--- root/cardgate/cardgate.php
+++ root/cardgate/cardgate.php
@@ -10,7 +10,7 @@ if (! defined ( '_PS_VERSION_' ))
 		var $shop_version = _PS_VERSION_;
 		
 		public function __construct() {
-			Configuration::updateValue ( 'CARDGATE_MODULE_VERSION', '1.7.5' );
+			Configuration::updateValue ( 'CARDGATE_MODULE_VERSION', '1.7.6' );
 			$this->name = 'cardgate';
 			$this->paymentcode = 'cardgate';
 			$this->paymentname = 'CardGate';
@@ -116,6 +116,9 @@ if (! defined ( '_PS_VERSION_' ))
 				Configuration::updateValue ( 'CARDGATE_MERCHANT_ID', $merchantid );
 				Configuration::updateValue ( 'CARDGATE_MERCHANT_API_KEY', $merchantapikey );
 				Configuration::updateValue ( 'CARDGATE_PAYMENT_DISPLAY', $paymentdisplay );
+				
+				// reset iDEAL issuer cache
+				Configuration::updateValue('cardgate_issuer_refresh', 0);
 				
 				$output .= $this->displayConfirmation ( $this->l('Settings updated' ) );
 			}
