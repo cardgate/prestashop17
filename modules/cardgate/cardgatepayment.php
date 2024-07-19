e3a10a17c0c49ef074efeda720172a48f84b995b
richard <richard@cardgate.com>
2024-07-19 13:43:04 +0200

    Removed Giropay payment method.

--- modules/cardgate/cardgatepayment.php
+++ modules/cardgate/cardgatepayment.php
@@ -4,7 +4,7 @@ use PrestaShop\PrestaShop\Core\Payment\PaymentOption;
 
 class CardgatePayment extends PaymentModule {
 
-    var $version = '1.7.15';
+    var $version = '1.7.16';
     var $tab = 'payments_gateways';
     var $author = 'CardGate';
     var $shop_version = _PS_VERSION_;
