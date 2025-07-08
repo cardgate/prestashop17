0eff1d679914747d4dc295219e2343ed8f82471b
richard <richard@cardgate.com>
2025-07-08 15:34:58 +0200

    Fix: Translation.

--- modules/cardgatecrypto/translations/nl.php
+++ modules/cardgatecrypto/translations/nl.php
@@ -2,8 +2,9 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_da9c72b9e543135f3f59e3c8ac68ef35'] = 'Betaal met';
-$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_b9a7a46e250290a3efd31ce1d269107a'] = 'CardGate Crypto';
-$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_590f4fe74d25d148b6097d67d5307e23'] = 'Accepteert betalingen met CardGate Crypto';
-$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_69a1a3ad8dd5da6db3c4da838a0cf9c7'] = 'Weet u zeker dat u uw details wilt verwijderen?';
-$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_59ca7aaff7f6bfd9dd776a6add44770f'] = 'De CardGate module is niet gevonden';
+$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_da9c72b9e543135f3f59e3c8ac68ef35'] = 'Betaal met ';
+$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_9c7b8f59860fe460fd00f19317d5ef6d'] = 'CardGate Crypto';
+$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_34e817d7aea3830da8bd4418fb054e24'] = 'Accepteert betalingen met Crypto';
+$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_69a1a3ad8dd5da6db3c4da838a0cf9c7'] = 'Weet u zeker dat u de details wilt wissen?';
+$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_a02758d758e8bec77a33d7f392eb3f8a'] = 'Er is geen valuta ingesteld voor deze module.';
+$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_59ca7aaff7f6bfd9dd776a6add44770f'] = 'De CardGate module is niet gevonden.';
