3fbdf7d2ccd177586427a2f11b7471bc4d1162b6
richard <richard@cardgate.com>
2025-07-08 14:30:09 +0200

    Added: Crypto payment method.

---
+++ modules/cardgatecrypto/translations/nl.php
@@ -0,0 +1,9 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_da9c72b9e543135f3f59e3c8ac68ef35'] = 'Betaal met';
+$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_b9a7a46e250290a3efd31ce1d269107a'] = 'CardGate Crypto';
+$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_590f4fe74d25d148b6097d67d5307e23'] = 'Accepteert betalingen met CardGate Crypto';
+$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_69a1a3ad8dd5da6db3c4da838a0cf9c7'] = 'Weet u zeker dat u uw details wilt verwijderen?';
+$_MODULE['<{cardgatecrypto}prestashop>cardgatecrypto_59ca7aaff7f6bfd9dd776a6add44770f'] = 'De CardGate module is niet gevonden';
